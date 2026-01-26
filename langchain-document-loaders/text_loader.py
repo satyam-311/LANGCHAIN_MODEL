ef0f3de588dad7e738f44312d00ef74c0f076077
Satyam Mishra <satyam3112003@gmail.com>
2026-01-27 03:24:04 +0530

    basic understanding of document loader

---
+++ langchain-document-loaders/text_loader.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(
+    model_name="llama-3.3-70b-versatile", # High performance Groq model
+    temperature=0.5
+)
+
+prompt = PromptTemplate(
+    template='Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader('langchain-document-loaders/cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
---
+++ langchain-document-loaders/webbase_loader.py
@@ -0,0 +1,29 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(
+    model_name="llama-3.3-70b-versatile", # High performance Groq model
+    temperature=0.5
+)
+
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://www.flipkart.com/apple-macbook-air-m2-16-gb-256-gb-ssd-macos-sequoia-mc7x4hn-a/p/itmdc5308fa78421'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'What is the prodcut that we are talking about?', 'text':docs[0].page_content}))
